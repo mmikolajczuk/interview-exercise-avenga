cbfd86b1cbae06b3b4d26f0e17020506b8c482f0
Maciej Mikolajczuk <maciej.mikolajczuk@gmail.com>
2021-09-22 10:13:42 +0200

    creating project

---
+++ src/main/java/pl/mmikolajczuk/interviewexercise/restapi/controller/CalculationController.java
@@ -0,0 +1,7 @@
+package pl.mmikolajczuk.interviewexercise.restapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculationController {
+}
